fc50238b00373b66d4e341a04d3cd0bc5778cfc3
SyedBashar <MhuammadBashar@gmail.com>
2024-10-15 19:37:56 +0500

    Username Task Done

---
+++ app.js
@@ -0,0 +1,8 @@
+var username =prompt("Enter Your Name")
+var password =prompt("Enter your Password")
+
+if(username==="admin" && password==="1234")
+{alert ("Login Successfully ")
+    
+}
+else{alert("Login Failed. Try Agian")}
